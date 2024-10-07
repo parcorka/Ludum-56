c39df05625b75966bd6d1bafcc18e9537b897566
Cizec <deadsasha1678643@gmail.com>
2024-10-08 01:42:59 +0300

    aaa 1

--- Assets/Scripts/Dialogue system/FirstQuest.cs
+++ Assets/Scripts/Dialogue system/FirstQuest.cs
@@ -10,61 +10,16 @@ public class FirstQuest : MonoBehaviour
 
     public QuestManager questManager;
     public Quest startQuest;
-    public Quest endQuest;
 
     private void Start()
     {
         if (questManager.GetMainQuestID() == myQuestID)
         {
-            dialogueSystem.StartDialogue(questDialogue);
-            //Debug.Log("Quest dialogue by " + questDialogue.name);
             StartCoroutine(WaitForDialogueEnd());
-            //Debug.Log("StartCoroutine by " + questDialogue.name);
         }
     }
-    //private void OnMouseOver()
-    //{
-    //    if (Vector3.Distance(player.transform.position, this.transform.position) <= 5f)
-    //    {
-    //        if (!dialogueSystem.GetDialogueState())
-    //        {
-    //            popUp.SetActive(true);
-    //            if (Input.GetKeyDown(KeyCode.E))
-    //            {
-    //                //Debug.Log(questManager.GetMainQuestID() + " == " + myQuestID);
-    //                if (questManager.GetMainQuestID() == myQuestID)
-    //                {
-    //                    dialogueSystem.StartDialogue(questDialogue);
-    //                    //Debug.Log("Quest dialogue by " + questDialogue.name);
-    //                    StartCoroutine(WaitForDialogueEnd());
-    //                    //Debug.Log("StartCoroutine by " + questDialogue.name);
-    //                }
-    //                else
-    //                {
-    //                    if (interactionCount == 0 && firstDialogue.sentences != null)
-    //                    {
-    //                        interactionCount++;
-    //                        dialogueSystem.StartDialogue(firstDialogue);
-    //                    }
-    //                    else
-    //                    {
-    //                        dialogueSystem.StartDialogue(secondDialogue);
-    //                    }
-    //                }
-    //            }
-    //        }
-    //        else
-    //        {
-    //            popUp.SetActive(false);
-    //        }
-    //    }
-    //}
     private void AdvanceQuest()
     {
-        if (endQuest != null && questManager != null)
-        {
-            questManager.CompliteQuest(endQuest);
-        }
         if (startQuest != null && questManager != null)
         {
             if (!startQuest.IsActive)
@@ -75,6 +30,8 @@ public class FirstQuest : MonoBehaviour
     }
     private IEnumerator WaitForDialogueEnd()
     {
+        yield return new WaitForSeconds(2f);
+        dialogueSystem.StartDialogue(questDialogue);
         yield return new WaitUntil(() => (!dialogueSystem.GetDialogueState()));
         //Debug.Log("WaitForDialogueEnd ended by " + questDialogue.name);
         AdvanceQuest();
